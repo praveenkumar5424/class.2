876933e7ab9c4b60430a840bc4c13aff96360d46
Praveen kumar.M <praveenkumar20026@gmail.com>
2025-09-15 07:54:13 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,22 @@
+public class starpattern {
+    public static void main(String[] args) {
+        for(int i=1; i<=4;i++){
+            for (int s=1; s<=4-i;s++){
+                System.out.print(" ");
+            }
+            
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+        for(int i=3; i>=1;i--){
+              for(int s=1;s<=4-i;s++){
+                System.out.print(" ");}
+                for (int j=1;j<=i;j++){
+                    System.out.print("*");
+                }System.out.println();
+            }
+    
+    
+}
+}
