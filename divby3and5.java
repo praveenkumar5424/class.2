6f7a01f6115172baa2ccb148be277282ea1615d4
Praveen kumar.M <praveenkumar20026@gmail.com>
2025-09-15 07:59:08 +0530

    Add files via upload

---
+++ divby3and5.java
@@ -0,0 +1,12 @@
+public class divby3and5 {
+    public static void main(String[] args) {
+        int n=48;
+            if(n%3==0 && n%4==0){
+                System.out.println("number is divible by both the numbers");
+            }
+            else{
+                System.out.println("number is not divisible");
+        }
+    }
+
+}
